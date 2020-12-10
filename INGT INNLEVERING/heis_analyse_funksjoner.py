2bd4a04f5cb59e4051fc5f4c0f84b5492af054f9
William M Schmidt <35806072+WilliamMRS@users.noreply.github.com>
2020-12-11 00:43:58 +0100

    Update heis_analyse_funksjoner.py

--- INGT INNLEVERING/heis_analyse_funksjoner.py
+++ INGT INNLEVERING/heis_analyse_funksjoner.py
@@ -69,12 +69,12 @@ def findHighestValues(window, day, df):
     
 # PLots a day from a dataframe.
 # needs a dataframe array, the day of the df-array, column. upper and lower shows minmax lines. hline will plot floors.
-def PlotDay(df, day_index, column, plotUpperAndLower, hline):
+def PlotDay(df, day_index, column, plotUpperAndLower, hline, name):
     start = 0
     stop = len(df[day_index][column])
 
     fig, ax=plt.subplots(figsize=(14,8))
-    plt.title("26. Novemeber")
+    plt.title(name)
     df[day_index].plot(x="Time", y=[column], ax=ax, color="green")
     ax.set_ylabel = column
     ax.set_xlim(start,stop)
@@ -104,14 +104,14 @@ def PlotDay(df, day_index, column, plotUpperAndLower, hline):
 floorLevels = [1016.3,1015.75,1015.25,1014.95,1014.55,1013.95,1013.5,1013.2,1012.8] 
 # 9 etasjer, bestemt ved hjelp av å trekke lineære vannrette linjer over punkt hvor dataene platået
 
-def createAdjustedGraphs(df, dataRange):
+def createAdjustedGraphs(df, dataRange, nameOfGraph):
     # finn stigningstallet til punktene fra min og max grafene og forskyv rådatapunktene basert på verdien til
     # min/max grafene i forhold til referansepunktet som er å slutten.
     novemberData = []
     df = df.reset_index()
     novemberData.append(df), novemberData.append(df), novemberData.append(df) # Dataframes som blir brukt nedenunder
     # ----------------------------- Plot raw data with minmax
-    minmaxseries = PlotDay(novemberData, 0, "Pressure", True, False)
+    minmaxseries = PlotDay(novemberData, 0, "Pressure", True, False, nameOfGraph)
     # Finner summen av topp og bunn snitt verdier. Ser på forskjellen mellom de 10 blokkene og den alle siste som brukes som referansepunkt
     referencePressureRange = minmaxseries[0][len(minmaxseries[0])-1] + minmaxseries[1][len(minmaxseries[1])-1]
     offsetvals = []
@@ -130,7 +130,7 @@ def createAdjustedGraphs(df, dataRange):
         else:
             i = 0 # reset i
             y += 1 # next window offset
-    PlotDay(novemberData, 1, "Pressure", True, False)
+    PlotDay(novemberData, 1, "Pressure", True, False, nameOfGraph)
     # ----------------------------- Om 0 er forskjøvet med 8 millibar og 1 er forskjøvet med 7 millibar er formelen:
     # newDatapoint = oldDatapoint + (8-7)/1800 * (1800-i), hvor i er det datapunktet som blir forskjøvet i denne rammen.
     i = 0
@@ -145,13 +145,13 @@ def createAdjustedGraphs(df, dataRange):
         else:
             i = 0 # reset i
             y += 1 # next window offset  
-    PlotDay(novemberData, 2, "Pressure", True, False)
+    PlotDay(novemberData, 2, "Pressure", True, False, nameOfGraph)
     cutNovemberData = []
     cutNovemberData.append(novemberData[2].iloc[0:dataRange])
-    PlotDay(cutNovemberData, 0, "Pressure", True, floorLevels)
+    PlotDay(cutNovemberData, 0, "Pressure", True, floorLevels, nameOfGraph)
     return cutNovemberData
 
-cutNovemberData = createAdjustedGraphs(data_from_all_days[0].iloc[0:20300], 20300)
+cutNovemberData = createAdjustedGraphs(data_from_all_days[0].iloc[0:20300], 20300, "1. Desember")
 
 # We've compensated for the weather changing the ambient pressure by finding the min-max range and
 # aligning the datapoints to a fixed reference point, in this case the last 1800 datapoints.
@@ -196,14 +196,11 @@ def modifyPressureGraph(): # Kind of digitizes the previous graph
     resetDf = cutNovemberData[0].reset_index(drop=True)
     digitizedNovemberData[0].insert(1, "Time", resetDf["Time"], True)
     return digitizedNovemberData
-
-#def createBarGraphOfFloors():
     
     
-
 data_from_all_days = separate_by_column_values(df, "Date")
 
-cutNovemberData = createAdjustedGraphs(data_from_all_days[7].iloc[0:20300], 20300)
+cutNovemberData = createAdjustedGraphs(data_from_all_days[7].iloc[0:20300], 20300, "26. November")
 
 digitizedNovemberData = modifyPressureGraph()
 digitizedNovemberData[0] = digitizedNovemberData[0][(digitizedNovemberData[0] != 0).all(1)] # Fjerner nullene
@@ -214,8 +211,6 @@ print(digitizedNovemberData[0]["Pressure"].value_counts())
 # Her kan vi telle antall datapunkt heisa har på ulike nivå. Slik kan vi se hvilke etasjer heisen tilbringer mest tid i.
 # Om vi kunne fått renere data kunne vi og funnet ut hvilken heisturer som er mest populære.
 # Vi kunne og estimert strømforbruken til heisa.
-
-
 
 
 
